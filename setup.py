87cce8ef4fa3fe860909fd673912c49a572c891f
mlovci <michaeltlovci@gmail.com>
2014-05-14 19:05:50 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,15 @@ setup(
     packages=find_packages(),
     url='',
     license='',
-    author='lovci',
+    author='',
     author_email='mlovci@ucsd.edu',
     description='',
     include_package_data=True,
-    #package_dir={'flotilla': 'flotilla'},
-    #    package_data={
-    #    },
-    #entry_points={},
+   
     install_requires=['setuptools',
                       'numpy >= 1.8.1 ',
                       'scipy >= 0.13.0',
                       'matplotlib >= 1.3.1',
-                      #'dcor_cpy' #needs to be built with extutils
     ],
-    version = "0.0.2"
+    version = "0.0.1"
 )
