3bade5ea0b6456d3d9d03605846ee6af7a01030b
Michael Lovci <michaeltlovci@gmail.com>
2014-05-14 17:50:38 -0700

    1

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'PyRPCA',
+    version = '',
+    packages = [''],
+    url = '',
+    license = '',
+    author = '',
+    author_email = '',
+    description = ''
+)
+src/pypropack
